1ef0a4ce31f0ebe61d3d8229a3a6986464b1bfcc
makau <makauwanyoike@gmail.com>
2026-05-27 12:34:49 +0300

    feat: update main function to handle file input and improve command processing

--- simpleshell.c
+++ simpleshell.c
@@ -3,11 +3,85 @@
  * main - main to the function
  * Return: 0
  */
-int main(void)
+int main(int argc, char **argv)
 {
 	char *command;
 	bin *head, *temp;
+	if (argc > 2)
+	{
+		write(2, "Usage: simple_shell [filename]\n", 30);
+		return (1);
+	}
 
+	if (argc == 2)
+	{
+		FILE *fp;
+		char *buffer = NULL;
+		size_t bufsize = 0;
+		ssize_t len;
+
+		fp = fopen(argv[1], "r");
+		if (fp == NULL)
+		{
+			perror("fopen");
+			return (1);
+		}
+
+		while ((len = _getline(&buffer, &bufsize, fp)) != -1)
+		{
+			ssize_t len1 = 0;
+			int i = 0;
+
+			if (len > 0 && buffer[len - 1] == '\n')
+				buffer[len - 1] = '\0';
+
+			while (buffer[i] != '#' && buffer[i] != '\0')
+			{
+				len1++;
+				i++;
+			}
+
+			command = malloc(len1 + 1);
+			if (command == NULL)
+			{
+				free(buffer);
+				fclose(fp);
+				handle_error("malloc");
+				exit(EXIT_FAILURE);
+			}
+			for (i = 0; i < len1; i++)
+				command[i] = buffer[i];
+			command[i] = '\0';
+
+			if (_strlen(command) == 0)
+			{
+				free(command);
+				continue;
+			}
+
+			head = command_node(command);
+			if (head == NULL)
+			{
+				free(command);
+				free(buffer);
+				fclose(fp);
+				return (-1);
+			}
+			temp = head;
+			while (temp != NULL)
+			{
+				execute_command(temp->dir);
+				temp = temp->next;
+			}
+			free(command);
+			free_node(head);
+		}
+		free(buffer);
+		fclose(fp);
+		return (0);
+	}
+
+	/* interactive mode */
 	while (1)
 	{
 		display_prompt();
